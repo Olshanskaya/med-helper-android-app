968e7ecf72ba7862caa5bbcd270285d0b970da4d
Olshanskaya <viktoriarubenovna@gmail.com>
2023-05-05 18:54:31 +0300

    start and log activity

---
+++ app/src/main/java/com/example/medhelperandroid/api/LoginResponse.java
@@ -0,0 +1,40 @@
+package com.example.medhelperandroid.api;
+
+public class LoginResponse {
+    private String message;
+    private String status;
+    private String timestamp;
+    private String error;
+    private String trace;
+    private String path;
+    private String email;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getTrace() {
+        return trace;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
--- app/src/main/java/com/example/medhelperandroid/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/medhelperandroid/ui/login/LoginActivity.java
@@ -10,15 +10,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.medhelperandroid.MainActivity;
 import com.example.medhelperandroid.R;
+import com.example.medhelperandroid.api.ApiService;
+import com.example.medhelperandroid.api.LoginRequest;
+import com.example.medhelperandroid.api.LoginResponse;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText mEmailField;
     private EditText mPasswordField;
     private Button mLoginButton;
+    private Button mNewPswButton;
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8080/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    ApiService apiService = retrofit.create(ApiService.class);
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -29,6 +49,17 @@ public class LoginActivity extends AppCompatActivity {
         mEmailField = findViewById(R.id.email_field);
         mPasswordField = findViewById(R.id.password_field);
         mLoginButton = findViewById(R.id.login_button);
+        mNewPswButton = findViewById(R.id.new_psw_button);
+
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,12 +68,37 @@ public class LoginActivity extends AppCompatActivity {
                 String password = mPasswordField.getText().toString();
 
                 // Здесь можно добавить код для проверки правильности введенной информации пользователя
+                LoginRequest loginRequest = new LoginRequest(email, password);
+                Call<LoginResponse> call = apiService.login(loginRequest);
+                call.enqueue(new Callback<LoginResponse>() {
+                    @Override
+                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            String emailReturned = response.body().getEmail();
+                            String status = response.body().getStatus();
+                            // После успешной проверки, можно запустить следующую активность
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish(); // Опционально закрываем эту активность, чтобы пользователь не мог вернуться к экрану входа по кнопке "Назад"
+                            // обработка ответа
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Введены неверные данные", Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
-                // После успешной проверки, можно запустить следующую активность
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish(); // Опционально закрываем эту активность, чтобы пользователь не мог вернуться к экрану входа по кнопке "Назад"
+                    @Override
+                    public void onFailure(Call<LoginResponse> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, "Что-то пошло не так, попробуйте позже", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
             }
+
+            ;
         });
+
     }
+
+    ;
 }
